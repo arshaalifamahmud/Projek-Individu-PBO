fe87c48cd20c571527810f0c2d3e7b3474e66ccb
arshaalifamahmud <arshaalifamahmud@gmail.com>
2025-12-01 18:28:25 +0700

    Penumpang.java

--- Penumpang.java
+++ Penumpang.java
@@ -11,3 +11,21 @@ public class Penumpang extends Person implements TopUp {
     public boolean getHamil() { return hamil; }
     public int getSaldo() { return saldo; }
 
+    @Override
+    public void tambahSaldo(int jumlah) {
+        if (jumlah > 0) saldo += jumlah;
+    }
+
+    public boolean isPrioritas() {
+        return umur > 60 || umur < 10 || hamil;
+    }
+
+    public void bayar(int ongkos) throws MyException {
+        if (saldo < ongkos) throw new MyException("Saldo tidak cukup!");
+        saldo -= ongkos;
+    }
+
+    public Jenis getJenis() {
+        return isPrioritas() ? Jenis.PRIORITAS : Jenis.BIASA;
+    }
+}
