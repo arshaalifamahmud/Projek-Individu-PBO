9f8480b45e9fa2ba64beb05810011e88e208de07
arshaalifamahmud <arshaalifamahmud@gmail.com>
2025-12-01 18:27:52 +0700

    Update Penumpang.java

--- Penumpang.java
+++ Penumpang.java
@@ -1,1 +1,13 @@
+public class Penumpang extends Person implements TopUp {
+    private boolean hamil;
+    private int saldo;
+
+    public Penumpang(int id, String nama, int umur, boolean hamil) {
+        super(id, nama, umur);
+        this.hamil = hamil;
+        this.saldo = 10000;   // saldo awal
+    }
+
+    public boolean getHamil() { return hamil; }
+    public int getSaldo() { return saldo; }
 
