b7393e4f8c98489d9d4639203eeb6b5c61ba3d1f
arshaalifamahmud <arshaalifamahmud@gmail.com>
2025-12-01 18:29:52 +0700

    Person.java

--- Person.java
+++ Person.java
@@ -1,1 +1,15 @@
+public abstract class Person {
+    protected int id;
+    protected String nama;
+    protected int umur;
 
+    public Person(int id, String nama, int umur) {
+        this.id = id;
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public int getID() { return id; }
+    public String getNama() { return nama; }
+    public int getUmur() { return umur; }
+}
