7c27ef6175f119ae1c79c9c34f873f0338726a8f
arshaalifamahmud <arshaalifamahmud@gmail.com>
2025-12-09 21:11:00 +0700

    Update TestBus.java

--- TestBus.java
+++ TestBus.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+public class TestBus {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Bus bus = new Bus();
+        int id = 1;
+
+        while (true) {
+            System.out.println("1. Naikkan Penumpang");
+            System.out.println("2. Turunkan Penumpang");
+            System.out.println("3. Lihat Penumpang");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih: ");
+            String p = in.nextLine();
 
